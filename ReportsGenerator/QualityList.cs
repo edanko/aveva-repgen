d774e6becdcf1f60f15d2e7021f0bcd4e8398255
Egor Danko <qwertyone@gmail.com>
2022-01-18 23:10:00 +0300

    Remove special density parsing because .net can parse scientific notation

--- ReportsGenerator/QualityList.cs
+++ ReportsGenerator/QualityList.cs
@@ -21,24 +21,8 @@ public static class QualityList
             }
 
             var s = l.Split(" ", StringSplitOptions.RemoveEmptyEntries);
-            var density = Regexp(s[2], "[0-9]+(?:\\.[0-9]*)?(?=E-)");
             res.Add(s[0], double.Parse(s[2], CultureInfo.InvariantCulture));
         }
         return res;
-    }
-
-    private static string Regexp(string s, string exp)
-    {
-        var regex = new Regex(exp);
-        var result = "";
-        var matchCollection = regex.Matches(s);
-        var num = 0;
-
-        var num2 = matchCollection.Count - 1;
-        for (var i = num; i <= num2; i++)
-        {
-            result = matchCollection[i].Value;
-        }
-        return result;
     }
 }
