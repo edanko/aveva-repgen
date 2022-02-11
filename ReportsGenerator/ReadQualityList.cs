293e1febf5ed6ed52033570f03b8091495320874
Egor Danko <qwertyone@gmail.com>
2022-02-12 02:14:55 +0300

    Fix density read for some quality list files

--- ReportsGenerator/ReadQualityList.cs
+++ ReportsGenerator/ReadQualityList.cs
@@ -7,6 +7,8 @@ public static class QualityList
     public static Dictionary<string, double> Read(string file)
     {
         var res = new Dictionary<string, double>();
+
+        double lastDensity = 0.0;
         
         var lines = File.ReadAllLines(file);
         foreach (var l in lines)
@@ -17,7 +19,15 @@ public static class QualityList
             }
 
             var s = l.Split(" ", StringSplitOptions.RemoveEmptyEntries);
-            res.Add(s[0], double.Parse(s[2], CultureInfo.InvariantCulture));
+            if (s[2] == "*")
+            {
+                res.Add(s[0], lastDensity);
+                continue;
+            }
+
+            var val = double.Parse(s[2], CultureInfo.InvariantCulture);
+            res.Add(s[0], val);
+            lastDensity = val;
         }
         return res;
     }
